98819ebef6dc5c65221f85287d941de883d0fa57
risalshahed <risalshahed@gmail.com>
2023-03-04 00:05:12 +0600

    first commit

---
+++ acumen.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       Acumen
+ * Plugin URI:        https://betterwizard.com/product/pay-what-you-want/
+ * Description:       A simple tool to add a variable payment option to your product so that people can buy your product with a given of sets of price.
+ * Version:           1.0.0
+ * Author:            Better Wizard
+ * Author URI:        https://betterwizard.com/
+ * Text Domain:       acumen
+ * Domain Path:       /languages
+ */
